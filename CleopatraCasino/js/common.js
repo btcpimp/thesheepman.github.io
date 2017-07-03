7ea30aecc0155c4ccac3be9705770f5281580afd
Andrey <andrtaw1@gmail.com>
2017-07-03 17:06:01 +0300

    cleopatra casino 1.0

---
+++ CleopatraCasino/js/common.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('.top-nav__search').click(function(event) {
+        $('.search-input').addClass('__search-visible')
+        $(this).addClass('__search-active')
+    });
+    $('.search-input__close').click(function(event) {
+        $('.search-input').removeClass('__search-visible')
+        $('.top-nav__search').removeClass('__search-active')
+    });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: true
+            },
+            1000: {
+                items: 4,
+                nav: true,
+            }
+        }
+    })
+    $('.owl-nav').prependTo('.wrapper__winners')
+    $('.owl-next').text('')
+    $('.owl-next').append('<i class="fa fa-angle-right" aria-hidden="true"></i>')
+    $('.owl-prev').text('')
+    $('.owl-prev').append('<i class="fa fa-angle-left" aria-hidden="true"></i>')
+    $('.show-more').click(function() {
+        event.preventDefault();
+        $('.block__preview ').removeClass('game-hidden')
+    });
+    $('.top-nav__mobile-button').click(function() {
+        event.preventDefault();
+        $('.mobile-nav').toggleClass('mobile-visible');
+    });
+});
