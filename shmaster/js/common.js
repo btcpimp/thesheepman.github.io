84d8a13525a7de18380510c63c738074b65a3c45
Andrey <andrtaw1@gmail.com>
2017-11-06 20:16:59 +0300

    shmaster products

--- shmaster/js/common.js
+++ shmaster/js/common.js
@@ -5,4 +5,20 @@ jQuery(document).ready(function($) {
 		menu.toggleClass('active');
 		$(this).toggleClass('menu-active');
 	});
+
+	//products
+	$('.procuts-item').click(function() {
+		$('.modal').toggleClass('active');
+		$('.products-item__title', this).toggleClass('active');
+		$('.products-inner', this).toggleClass('active');
+	});
+
+	$('.modal').click(function() {
+		if($(this).hasClass('active')){
+			$(this).toggleClass('active');
+			$('.products-inner').removeClass('active')
+			$('.products-item__title').removeClass('active')
+
+		}
+	});
 });
