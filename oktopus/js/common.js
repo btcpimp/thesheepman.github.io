9a5d64a6a839dc75c6da638d4f2d65df15fc17c0
Andrey <andrtaw1@gmail.com>
2018-06-01 22:58:28 +0300

    oktopus v1.0

---
+++ oktopus/js/common.js
@@ -0,0 +1,322 @@
+AOS.init();
+var dappAddress = "n1dxDzLsLW5vsEUoiqSw8jX7uUvs86p1S88";
+
+var NebPay = require("nebpay");
+var nebPay = new NebPay();
+if (typeof(webExtensionWallet) === "undefined") {
+
+    window.postMessage({
+        "target": "contentscript",
+        "data": {},
+        "method": "getAccount",
+    }, "*");
+    window.addEventListener('message', function(e) {
+        // e.detail contains the transferred data (can
+        console.log("recived by page:" + e + ", e.data:" + JSON.stringify(e.data));
+        if (!!e.data.data) {
+            $('.account-address').text(e.data.data.account)
+        }
+    });
+} else {
+
+
+}
+
+
+var isWithdraw = false
+var isDeposit = false
+/** Preload shared songs */
+function getBalance() {
+    var args = '';
+    var func = "getBalance";
+    nebPay.simulateCall(dappAddress, 0, func, args, {
+        listener: getBalanceResp
+    });
+}
+
+function getBalanceResp(resp) {
+    if (resp.result != "Error: You haven't created vault yet.") {
+        var result = eval(JSON.parse(resp.result));
+        result = result / 1000000000000000000
+        Vue.set(app, 'balance', result)
+        if (result <= 0) {
+            isWithdraw = false
+            Vue.set(app, 'withdraw', false)
+        } else {
+            isWithdraw = true
+            Vue.set(app, 'withdraw', true)
+        }
+    } else {
+        Vue.set(app, 'balance', 0)
+    }
+}
+
+function getInfo() {
+    var args = '';
+    var func = "getInfo";
+    nebPay.simulateCall(dappAddress, 0, func, args, {
+        listener: getInfoResp
+    });
+}
+
+function getInfoResp(resp) {
+    if (resp.result != "Error: You haven't created vault yet.") {
+        var result = eval(JSON.parse(resp.result));
+        Vue.set(app, 'name', result.name)
+        Vue.set(app, 'expDate', result.expDate)
+        if (result.name = '') {
+            Vue.set(app, 'deposit', false)
+            isDeposit = false
+        } else {
+            Vue.set(app, 'deposit', true)
+            isDeposit = true
+        }
+    } else {
+        return false
+    }
+}
+
+
+getInfo()
+getBalance()
+
+var args = ""
+
+
+
+function createMoneybox() {
+
+
+    createBank();
+}
+
+
+function createBank() {
+    var inputDate = $('.md-input').val()
+    var name = $('.moneybox-name').val()
+    var date = new Date(inputDate);
+
+    var month = date.getMonth() + 1
+    var day = date.getDate()
+    var year = date.getFullYear()
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+    var args2 = '[' + '"' + name + '",' + '"' + day + '",' + '"' + month + '",' + '"' + year + '"' + ']';
+    var func = "createBank";
+    nebPay.simulateCall(dappAddress, 0, func, args2, {
+        listener: createBankResp
+    });
+}
+
+function createBankTrue() {
+    var inputDate = $('.md-input').val()
+    var name = $('.moneybox-name-input').val()
+    var date = new Date(inputDate);
+
+    var month = date.getMonth() + 1
+    var day = date.getDate()
+    var year = date.getFullYear()
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    var args2 = '[' + '"' + name + '",' + '"' + day + '",' + '"' + month + '",' + '"' + year + '"' + ']';
+    var func = "createBank";
+    nebPay.call(dappAddress, 0, func, args2, {
+        listener: createBankRespTrue
+    });
+}
+
+function createBankResp(resp) {
+    if (
+        resp.result === "Error: Name must contain at least 1 symbol." || resp.result === "Error: You have already setup a timer" || resp.result === "Error: you must enter a future date" || resp.result === "Error: Enter date in a dd/mm/yyyy format."
+    ) {
+
+        $('.add-tooltip').removeClass('hide')
+        $('.add-tooltip').addClass('--fail')
+        $(".add-tooltip").text(resp.result);
+    } else {
+        createBankTrue();
+    }
+}
+
+function createBankRespTrue(resp) {
+    if (resp != "Error: Transaction rejected by user") {
+        $('.add-tooltip').removeClass('hide')
+        $('.add-tooltip').removeClass('--fail')
+        $(".add-tooltip").text("You have succefully created moneybox.");
+        Vue.set(app, 'deposit', true)
+    } else {
+        $('.add-tooltip').removeClass('hide')
+        $('.add-tooltip').addClass('--fail')
+        $('.add-tooltip').text("You have rejected transaction.");
+    }
+
+}
+
+function deposit() {
+
+    addFunds()
+}
+
+
+function depoActivate(event) {
+    if (isDeposit === true) {
+        getInfo()
+        getBalance()
+        Vue.set(app, 'activeItem', 'deposit')
+    } else {
+        return false
+    }
+
+}
+
+function withdrawActivate() {
+    if (isWithdraw === true) {
+        getInfo()
+        getBalance()
+        Vue.set(app, 'activeItem', 'withdraw')
+    } else {
+        return false
+    }
+}
+
+function addFunds() {
+    var inputDate = $('.deposit').val().trim()
+    var args2 = ''
+    var func = "addFunds";
+    nebPay.simulateCall(dappAddress, inputDate, func, args2, {
+        listener: addFundsResp
+    });
+}
+
+function addFundsTrue() {
+    var inputDate = $('.deposit').val().trim()
+    var args2 = ''
+    var func = "addFunds";
+    nebPay.call(dappAddress, inputDate, func, args2, {
+        listener: addFundsRespTrue
+    });
+}
+
+function addFundsResp(resp) {
+    if (
+        resp.result === "Error: You haven't created vault yet." || resp.result === 'Error: Your deposit must be more than 0.'
+    ) {
+        $('.deposit-tooltip').removeClass('hide')
+        $('.deposit-tooltip').addClass('--fail')
+        $(".deposit-tooltip").text(resp.result);
+    } else {
+        addFundsTrue();
+    }
+}
+
+function addFundsRespTrue(resp) {
+    if (resp != "Error: Transaction rejected by user") {
+        $('.deposit-tooltip').removeClass('hide')
+        $('.deposit-tooltip').removeClass('--fail')
+        $(".deposit-tooltip").text("You have succefully deposit NAS.");
+        Vue.set(app, 'withdraw', true)
+        isWithdraw = true
+    } else {
+        $('.deposit-tooltip').removeClass('hide')
+        $('.deposit-tooltip').addClass('--fail')
+        $('.deposit-tooltip').text("You have rejected transaction.");
+    }
+
+
+}
+
+
+function withdrawFunds() {
+
+    withdraw();
+
+}
+
+function withdraw() {
+    var args2 = ''
+    var func = "withdraw";
+    nebPay.simulateCall(dappAddress, 0, func, args2, {
+        listener: withdrawResp
+    });
+}
+
+function withdrawTrue() {
+    var args2 = ''
+    var func = "withdraw";
+    nebPay.call(dappAddress, 0, func, args2, {
+        listener: withdrawRespTrue
+    });
+}
+
+function withdrawResp(resp) {
+    if (
+        resp.result != "true"
+    ) {
+        $('.withdraw-tooltip').removeClass('hide')
+        $('.withdraw-tooltip').addClass('--fail')
+        $(".withdraw-tooltip").text(resp.result);
+    } else {
+        withdrawTrue();
+    }
+}
+
+function withdrawRespTrue(resp) {
+    if (resp != "Error: Transaction rejected by user") {
+        $('.withdraw-tooltip').removeClass('hide')
+        $('.withdraw-tooltip').removeClass('--fail')
+        $(".withdraw-tooltip").text("You have succefully withdraw NAS.");
+        Vue.set(app, 'deposit', false)
+        isWithdraw = false
+        isDeposit = false
+        setTimeout(Vue.set(app, 'withdraw', false), 5000)
+        getInfo()
+        getBalance()
+    } else {
+        $('.withdraw-tooltip').removeClass('hide')
+        $('.withdraw-tooltip').addClass('--fail')
+        $('.withdraw-tooltip').text("You have rejected transaction.");
+    }
+
+
+}
+Vue.use(VueMaterial.default)
+
+var app = new Vue({
+
+    beforeMount() {
+
+
+    },
+    mounted() {
+        setTimeout(() => {
+            this.isLoading = false
+        }, 1500)
+    },
+
+    data: {
+        songs: [],
+        activeItem: 'start',
+        balance: '',
+        expDate: '',
+        webExt: false,
+        name: '',
+        withdraw: false,
+        deposit: false,
+        isLoading: true
+    },
+
+    methods: {
+
+    }
+})
+
+app.$mount("#app")
---
+++ oktopus/smartContract/oktopus.js
@@ -0,0 +1,131 @@
+"use strict";
+var oktopus = function() {
+    LocalContractStorage.defineMapProperty(this, "bank")
+    LocalContractStorage.defineMapProperty(this, "account")
+    LocalContractStorage.defineProperty(this, "ranksCounter", null)
+}
+
+oktopus.prototype = {
+    init: function() {
+        this.songsCounter = 0
+    },
+    createBank: function(name, date, month, year) {
+        var from = Blockchain.transaction.from;
+        name = name.trim();
+        if (name === ""){
+        	throw new Error("Name must contain at least 1 symbol.");
+        }
+        if (this.account.get(from)) {
+            throw new Error("You have already setup a timer");
+        } else {
+            var expDate = date + '/' + month + '/' + year
+        }
+        if (expDate.match(/(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)/)) {
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1; //January is 0!
+
+            var yyyy = today.getFullYear();
+            if (dd < 10) {
+                dd = '0' + dd;
+            }
+            if (mm < 10) {
+                mm = '0' + mm;
+            }
+            if (yyyy > year) {
+                throw new Error("you must enter a future date");
+
+            } else if (mm > month) {
+                throw new Error("you must enter a future date");
+            } else if (dd > date) {
+                throw new Error("you must enter a future date");
+            }
+            this.account.set(from, {name: name, expDate: expDate, day: date, month: month, year: year })
+            var startAmount = new BigNumber(0)
+            this.bank.set(from, startAmount)
+            return true
+
+        } else {
+            throw new Error("Enter date in a dd/mm/yyyy format.");
+        }
+    },
+    addFunds: function() {
+        var from = Blockchain.transaction.from;
+        var value = Blockchain.transaction.value;
+        if (!this.account.get(from)) {
+            throw new Error("You haven't created vault yet.");
+        }
+        if (value <= 0 ){
+            throw new Error("Your deposit must be more than 0.");
+        }
+
+        var newDepo = value.plus(this.bank.get(from))
+        this.bank.del(from)
+        this.bank.set(from, newDepo)
+        return true
+    },
+    withdraw: function() {
+        var from = Blockchain.transaction.from;
+
+        if (this.account.get(from)) {
+            if (this.bank.get(from) > 0) {
+
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth() + 1; //January is 0!
+
+                var yyyy = today.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd;
+                }
+                if (mm < 10) {
+                    mm = '0' + mm;
+                }
+                if (yyyy < this.account.get(from).year) {
+                    throw new Error("You should wait until " + this.account.get(from).expDate);
+
+
+
+                } else if (mm < this.account.get(from).month) {
+                    throw new Error("You should wait until " + this.account.get(from).expDate);
+
+                } else if (dd < this.account.get(from).day) {
+                    throw new Error("You should wait until " + this.account.get(from).expDate);
+                }
+
+                var amount = new BigNumber(this.bank.get(from))
+                var result = Blockchain.transfer(from, amount);
+                if (!result) {
+                    throw new Error("transfer failed.");
+                }
+                this.account.del(from)
+                this.bank.del(from)
+                return true
+            } else {
+                throw new Error("You didnt't add funds yet.");
+            }
+        } else {
+            throw new Error("You haven't created vault yet.");
+        }
+    },
+    getBalance: function() {
+        var from = Blockchain.transaction.from;
+        if (this.bank.get(from)) {
+            return this.bank.get(from)
+        } else {
+            throw new Error("You haven't created vault yet.");
+        }
+
+    },
+    getInfo: function() {
+        var from = Blockchain.transaction.from;
+        if (this.account.get(from)) {
+        	return this.account.get(from)
+        } else {
+            throw new Error("You haven't created vault yet.");
+        }
+
+    }
+}
+
+module.exports = oktopus
